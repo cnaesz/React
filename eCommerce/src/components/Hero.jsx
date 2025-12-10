56eabbc544b1d3b8588d760a475a68506d48bde3
Habib eslami <sina.eslami99@gmail.com>
2025-12-10 22:50:42 +0330

    added figma, cat from api

--- eCommerce/src/components/Hero.jsx
+++ eCommerce/src/components/Hero.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
+import useApi from '../hooks/useApi';
+
 import './hero.css';
 
 export const Hero = () => {
+  const { data, loading, error } = useApi('https://api.escuelajs.co/api/v1/categories');
+
+  if (loading) console.log('loading');
+  if (error) return console.log('err');
+
   return (
     <div className='hero'>
       <div className='hero-img'>
         <img src='./heroimg.jpg'></img>
       </div>
       <div className='herocat'>
-        <ul className='it'>
-          Woman's Fashio <i>></i>
-        </ul>
-        <ul className='it'>
-          Woman's Fashio<i>></i>
-        </ul>
-        <ul className='it'>
-          Woman's Fashio<i>></i>
-        </ul>
-        <ul className='it'>Woman's Fashio</ul>
-        <ul className='it'>Woman's Fashio</ul>
-        <ul className='it'>Woman's Fashio</ul>
-        <ul className='it'>Woman's Fashio</ul>
-        <ul className='it'>Woman's Fashio</ul>
-        <ul className='it'>Woman's Fashio</ul>
+        {data
+          .sort(() => Math.random() - 0.5)
+          .slice(0, 9)
+          .map((product) => (
+            <ul className='it' key={product.id}>
+              {product.name} <i>></i>
+            </ul>
+          ))}
       </div>
     </div>
   );
